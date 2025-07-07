2a7a2459b5697ff61271ff97193bf1c0fbe42456
Felipeferrari0 <feferrari@ibm.com>
2025-07-07 11:57:44 -0300

    Add files via upload

---
+++ terraform/main.tf
@@ -0,0 +1,74 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_key_pair" "neobank_key" {
+  key_name   = "neobank-key"
+  public_key = file("C:/Users/FelipeFerrari/OneDriveIBM/Documentos/NeoBank/keys/neobank-key.pub")
+}
+
+
+resource "aws_security_group" "allow_ssh_http" {
+  name        = "allow_ssh_http"
+  description = "Allow SSH and HTTP"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description      = "Allow SSH from anywhere"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []
+    security_groups  = []
+    self             = false
+    prefix_list_ids  = []
+  }
+
+  ingress {
+    description      = "Allow HTTP from anywhere"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []
+    security_groups  = []
+    self             = false
+    prefix_list_ids  = []
+  }
+
+  egress {
+    description      = "Allow all outbound"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []
+    security_groups  = []
+    self             = false
+    prefix_list_ids  = []
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                         = "ami-0c803b171269e2d72"
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.neobank_key.key_name
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.allow_ssh_http.id]
+
+  tags = {
+    Name = "neobank"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+echo "[web]" > ../ansible/inventory.ini
+echo "${self.public_ip} ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/neobank-key.pem" >> ../ansible/inventory.ini
+EOT
+  }
+}
