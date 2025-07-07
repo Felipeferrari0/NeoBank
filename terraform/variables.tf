5cc45745afc35b0f9f9432adfbddf7f7a84d7214
Felipeferrari0 <feferrari@ibm.com>
2025-07-07 12:03:20 -0300

    variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,11 @@
-# variables.tf (opcional)
+variable "aws_region" {
+  description = "AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "instance_type" {
+  description = "Instance type for EC2"
+  type        = string
+  default     = "t2.micro"
+}
