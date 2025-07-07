4535594310ff7fd9bdc7ce292076ea330bfe0cee
Felipeferrari0 <feferrari@ibm.com>
2025-07-07 12:04:26 -0300

    main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,7 +56,7 @@ resource "aws_security_group" "allow_ssh_http" {
 
 resource "aws_instance" "web" {
   ami                         = "ami-0c803b171269e2d72"
-  instance_type               = "t2.micro"
+  instance_type               = var.instance_type
   key_name                    = aws_key_pair.neobank_key.key_name
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.allow_ssh_http.id]
